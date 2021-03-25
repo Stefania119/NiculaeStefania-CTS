bfc480f7b1c9236b17ee8261598eb3b05aeecf6a
Stefania119 <72298824+Stefania119@users.noreply.github.com>
2021-03-25 16:17:39 +0200

    Add files via upload

---
+++ seminar5/src/ro/ase/cts/simplefactory/program/Main.java
@@ -0,0 +1,29 @@
+package ro.ase.cts.simplefactory.program;
+
+import ro.ase.cts.simplefactory.clase.FactoryPersonal;
+import ro.ase.cts.simplefactory.clase.Medic;
+import ro.ase.cts.simplefactory.clase.PersonalSpital;
+import ro.ase.cts.simplefactory.clase.TipPersonal;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		try {
+			FactoryPersonal factory=new FactoryPersonal();
+			PersonalSpital medic=factory.getPersonal(TipPersonal.MEDIC, "Ana");
+			PersonalSpital asistent=factory.getPersonal(TipPersonal.ASISTENT,"Gigi");
+			PersonalSpital brancardier=factory.getPersonal(TipPersonal.BRANCARDIER, "Mimi");
+			System.out.println(medic.toString());
+			System.out.println(asistent.toString());
+			System.out.println(brancardier.toString());
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+
+	}
+
+}
